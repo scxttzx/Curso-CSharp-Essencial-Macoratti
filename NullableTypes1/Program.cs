74b04bd5acf776c32cd0e22a478fdff18e2adcc2
crfmattos <gabrielmbserra@gmail.com>
2024-03-19 13:50:33 -0300

    fazendo o curso

---
+++ NullableTypes1/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("## Nullable Types ##");
+
+int valor = 100;
+Console.WriteLine(valor);
+
+// int valor1 = null;
+
+Nullable<int> i = null;
+Nullable<double> d = null;
+Nullable<bool> b = null;
+
+// Ouu
+
+int? a = null;
+double? c = null;
+bool? t = null;
+
+Console.WriteLine(i);
+Console.WriteLine(d);
+Console.WriteLine(b);
+
+
+int? z = null;
+// int x = z;
+int x = z ?? 0;
+
+Console.WriteLine(x);
+
+int? g = 100;
+
+if(g.HasValue)
+{
+    Console.WriteLine($"g = {g.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor (null)");
+}
+
+Console.ReadKey();
