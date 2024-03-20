0c003a9ac3258efee5a7057eacf4e0a617092f0b
crfmattos <gabrielmbserra@gmail.com>
2024-03-20 13:58:40 -0300

    aaa

---
+++ Identificadores/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Definindo Identificadores");
+Console.WriteLine();
+
+// Identificadores válidos
+string nome;
+string nomeCompleto;
+int idade;
+int _valor;
+int idade1;
+
+// Identificadores inválidos
+int 5idade;
+int $valor;
+int valor#total;
+
