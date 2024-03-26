2224b68c87639b9dd1826dfd619d0ddf672d79bd
crfmattos <gabrielmbserra@gmail.com>
2024-03-25 23:00:31 -0300

    aulas c#

--- Identificadores/Program.cs
+++ Identificadores/Program.cs
@@ -9,7 +9,28 @@ int _valor;
 int idade1;
 
 // Identificadores inválidos
-int 5idade;
-int $valor;
-int valor#total;
+//int 5idade;
+//int $valor;
+//int valor#total;
+//string nome Completo;
+
+// Para nomes de variáveis : camel case
+string descontoTotal;
+string desconto_Total;
+
+// Constantes : Maiúsculas
+double PI = 3.14;
+string PREFIXO = "";
+string PREFIXO_SP = "11";
+
+Console.ReadLine();
+
+// Para nomes de classe e métodos: pascal case
+class ImprimirTexto()
+{
+    public void ImpimeNome()
+    {
+        Console.WriteLine("Macoratti");
+    }
+}
 
---
+++ SaidaDados/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Saída de dados : Formatação");
+Console.WriteLine();
+
+int idade = 25;
+string nome = "Maria";
+Console.Write(nome);
+Console.Write(idade);
+Console.WriteLine();
+
+// Escreve na mesma linha 'Maria tem 25 anos'
+Console.Write(nome);
+Console.Write(" tem ");
+Console.Write(idade);
+Console.Write(" anos ");
+Console.WriteLine();
+Console.WriteLine("---------Concatenação-----------");
+
+// Usando a concatenação : usando o operador +
+Console.WriteLine(nome + " tem " + idade + " anos");
+Console.Write(nome + " tem " + idade + " anos");
+Console.WriteLine();
+Console.WriteLine("----------Interpolação-----------");
+// Usa a interpolação de strings : $ -> a interpolação {}
+Console.WriteLine($"{nome} tem {idade} anos");
+
+Console.WriteLine("----------Place holders-----------");
+// Usar place holders : usa {} com numeração com inicio em zero
+Console.WriteLine("{0} tem {1} anos", nome, idade);
+Console.ReadLine();
